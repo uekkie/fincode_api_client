acfa8b528b380109140d4b74ea26e5f6a62c709d
Hiroshi Ueki <ueki@sonicgarden.jp>
2025-02-19 13:45:41 +0900

    fix: 決済手段登録時のレスポンスでは access_id はnullableとする

--- fincode_api_client/lib/fincode_api_client/models/payment_method_card.rb
+++ fincode_api_client/lib/fincode_api_client/models/payment_method_card.rb
@@ -346,11 +346,8 @@ module FincodeApiClient
     # Custom attribute writer method with validation
     # @param [Object] access_id Value to be assigned
     def access_id=(access_id)
-      if access_id.nil?
-        fail ArgumentError, 'access_id cannot be nil'
-      end
 
-      if access_id.to_s.length > 24
+      if !access_id.nil? && access_id.to_s.length > 24
         fail ArgumentError, 'invalid value for "access_id", the character length must be smaller than or equal to 24.'
       end
 
